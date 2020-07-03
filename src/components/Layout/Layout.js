f5ace2e4de435ac52f0f820fe3bb0fdeb9a0c72e
tearofenemy <calm.snow98@gmail.com>
2020-07-03 23:46:13 +0300

    create ingredients for build burger

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 
 const layout = props => {
     return(
         <Aux>
             <div>
                 <div>Toolbar, Sidedraw, Backdrop</div>
-                <main>{props.children}</main>
+                <main className={classes.Content}>{props.children}</main>
             </div>
         </Aux>
     )
