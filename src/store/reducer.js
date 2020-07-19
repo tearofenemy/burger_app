e089b3e22e6b7443b52ae87541458f317dcf10a1
tearofenemy <calm.snow98@gmail.com>
2020-07-19 04:38:37 +0300

    install redux, setup store

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionType from './actions';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 0
+};
+
+const reducer = (state = initialState, action) => {
+    return state;
+};
+
+export default reducer;
